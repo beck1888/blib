8d92d18286e9261c01ff131bf1a79cce5fe211b4
Beck Orion <beckorion@gmail.com>
2025-07-20 18:03:05 -0700

    Enhance one_off_prompt function to accept optional system prompt and streamline response handling

--- apis/openai.py
+++ apis/openai.py
@@ -1,33 +1,42 @@
 from openai import OpenAI
 
-def one_off_prompt(prompt: str, api_key: str = None) -> str:
+def one_off_prompt(prompt: str, api_key: str = None, **kwargs) -> str:
     """Allows you to quickly send a prompt to gpt-4o if all you need is a quick ai response. 
     Make sure to pass an API key if it's not loaded in your env."""
 
-
+    # Init the client with the api key from env if not passed directly
     if api_key is None:
         client = OpenAI()
     else:
         # if a key was passed
         client = OpenAI(api_key=api_key)
 
-    response = client.chat.completions.create(
-        model='gpt-4o',
-        messages=[
+    # Create the chat history (messages)
+    chat_history = [
             {
                 "role": "user",
                 "content": prompt
             }
-        ]
+    ]
+
+    # Appending optional system prompt
+    if "system_prompt" in kwargs:
+        system_prompt = kwargs["system_prompt"]
+        chat_history.append(
+            {
+                "role": "system",
+                "content": system_prompt
+            }
+        )
+        chat_history.reverse() # Puts the system prompt at the start
+
+    # Call the API
+    response = client.chat.completions.create(
+        model='gpt-4o',
+        messages=chat_history
     ).choices[0].message.content
 
-    # Filter response
-    import string
-    okay = 'abcdefghijklmnopqrstuvwxyz0123456789!?"' + "'"
+    # Make sure the client closes
+    client.close()
 
-    final_response = ''
-    for char in response:
-        if char in okay:
-            final_response += char
-
-    return final_response
+    return response
