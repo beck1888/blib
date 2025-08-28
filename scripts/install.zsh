43545948508587a04b1269d798bbbc050463f7d4
Beck <123674650+geckoHex@users.noreply.github.com>
2025-08-28 14:29:24 -0700

    Update script URL in install.zsh

--- scripts/install.zsh
+++ scripts/install.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-# Script URL: https://raw.githubusercontent.com/beck1888/blib/refs/heads/main/scripts/install.zsh
+# Script URL: https://raw.githubusercontent.com/geckoDev/blib/refs/heads/main/scripts/install.zsh
 
 # Colors
 GREEN='\033[0;32m'
